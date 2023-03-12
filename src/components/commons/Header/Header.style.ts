afd61d589deee9c10d214ef9c89149c07ff86ca2
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2023-03-12 20:22:52 +0900

    [#3] feat: useInfiniteQuery를 사용해 무한 스크롤 구현

--- src/components/commons/Header/Header.style.ts
+++ src/components/commons/Header/Header.style.ts
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import Box from "../../../styles/box";
+import { palette } from "../../../styles/palette";
 
 export const Base = styled.div`
   position: sticky;
@@ -7,12 +8,13 @@ export const Base = styled.div`
   left: 0px;
   right: 0px;
   margin: 0px auto;
-  max-width: 768px;
   transition: top 300ms cubic-bezier(0.37, 0, 0.63, 1) 0s;
+  z-index: 1;
+  background-color: ${palette.gray[100]};
 `;
 
 export const Container = styled(Box)`
   padding: 20px 20px 15px;
-  max-width: 768px;
+
   height: 50px !important;
 `;
--- src/components/commons/Layout/Layout.style.ts
+++ src/components/commons/Layout/Layout.style.ts
@@ -1,9 +1,7 @@
 import styled from "@emotion/styled";
 
 export const Base = styled.div`
-  max-width: 768px;
   min-height: 800px;
-  z-index: 1;
   box-sizing: border-box;
   position: relative;
   float: none;
